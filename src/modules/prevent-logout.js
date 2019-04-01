1a1a1443c6b0b62c068471747a692b8da09c2f89
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2019-04-01 14:49:10 +0200

    Fixes prevent-logout not working

--- src/modules/prevent-logout.js
+++ src/modules/prevent-logout.js
@@ -1,31 +1,43 @@
 (function() {
-  var checkForActiveStatusAndResetTimer = function (){
+  var checkForActiveStatusAndResetTimer = function() {
     var xhrStatus = new XMLHttpRequest();
-    xhrStatus.open('GET', '/p470014-web/api/session/current', true)
-    xhrStatus.send()
+    var baseUrl = window.location.pathname
+      .split("/")
+      .slice(0, -1) // Removes last element
+      .join("");
+
+    xhrStatus.open("GET", `/${baseUrl}/api/session/current`, true);
+    xhrStatus.send();
     xhrStatus.onreadystatechange = function(e) {
       if (xhrStatus.readyState == 4 && xhrStatus.status == 200) {
-        var response = JSON.parse(xhrStatus.responseText)
+        var response = JSON.parse(xhrStatus.responseText);
         if (response.active) {
-          var xhrRenew = new XMLHttpRequest()
-          xhrRenew.open('GET', '/p470014-web/api/session/current?renew=true', true)
-          xhrRenew.send()
+          var xhrRenew = new XMLHttpRequest();
+          xhrRenew.open(
+            "GET",
+            `/${baseUrl}/api/session/current?renew=true`,
+            true
+          );
+          xhrRenew.send();
           xhrRenew.onreadystatechange = function(e) {
             if (xhrRenew.readyState == 4 && xhrRenew.status == 200) {
-              var renewResponse = JSON.parse(xhrRenew.responseText)
-              console.log(renewResponse)
+              var renewResponse = JSON.parse(xhrRenew.responseText);
+              console.log(renewResponse);
             }
-          }
-        }    
+          };
+        }
+      }
+    };
+  };
+
+  chrome.storage.sync.get(
+    {
+      preventAutomaticLogout: ""
+    },
+    function(items) {
+      if (items.preventAutomaticLogout) {
+        setInterval(checkForActiveStatusAndResetTimer, 1000 * 60 * 5); //5 minutes
       }
     }
-  }
-
-  chrome.storage.sync.get({
-    preventAutomaticLogout: ''
-  }, function(items) {
-    if(items.preventAutomaticLogout) {
-      setInterval(checkForActiveStatusAndResetTimer, 1000*60*5) //5 minutes
-    }
-  })
-})()
+  );
+})();
